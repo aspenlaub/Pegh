251d8ba3f3d766b604d99c049c7dfd56e779a36e
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2017-06-12 18:01:50 +0200

    Pegh.Interfaces was missing in assembly repository

--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
 ﻿using Aspenlaub.Net.GitHub.CSharp.Pegh.Components;
+using Aspenlaub.Net.GitHub.CSharp.Pegh.Interfaces;
 
 namespace Aspenlaub.Net.GitHub.CSharp.Pegh {
     internal class Program {
         private static void Main() {
             var componentProvider = new ComponentProvider();
             componentProvider.AssemblyRepository.AddToRepositoryIfNecessary(typeof(ComponentProvider));
+            componentProvider.AssemblyRepository.AddToRepositoryIfNecessary(typeof(IComponentProvider));
         }
     }
 }
