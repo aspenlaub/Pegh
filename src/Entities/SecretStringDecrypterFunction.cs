affab1768e0dfd20718ad10e3ff6100d6a7cba31
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2018-06-20 16:12:46 +0200

    Secret string encrypter/decrypter

---
+++ src/Entities/SecretStringDecrypterFunction.cs
@@ -0,0 +1,22 @@
+﻿using Aspenlaub.Net.GitHub.CSharp.Pegh.Interfaces;
+
+namespace Aspenlaub.Net.GitHub.CSharp.Pegh.Entities {
+    public class SecretStringDecrypterFunction : ISecret<PowershellFunction<string, string>> {
+        private static PowershellFunction<string, string> vDefaultPowershellFunction;
+        public PowershellFunction<string, string> DefaultValue {
+            get {
+                return vDefaultPowershellFunction ??
+                       (vDefaultPowershellFunction = new PowershellFunction<string, string> {
+                           FunctionName = "Decrypt-String",
+                           Script = "\r\n\tfunction Decrypt-String($s) {\r\n"
+                                    + "\t\t$result = New-Object -TypeName \"Aspenlaub.Net.GitHub.CSharp.Pegh.Entities.PowershellFunctionResult\"\r\n"
+                                    + "\t\t$result.Result = $s.Substring(0, $s.Length - 25)\r\n"
+                                    + "\t\treturn $result\r\n"
+                                    + "\t}\r\n"
+                       });
+            }
+        }
+
+        public string Guid { get { return "F2239CAF-A303-45A1-A0CA-B20CEAFB8CC1"; } }
+    }
+}
---
+++ src/Entities/SecretStringEncrypterFunction.cs
@@ -0,0 +1,22 @@
+﻿using Aspenlaub.Net.GitHub.CSharp.Pegh.Interfaces;
+
+namespace Aspenlaub.Net.GitHub.CSharp.Pegh.Entities {
+    public class SecretStringEncrypterFunction : ISecret<PowershellFunction<string, string>> {
+        private static PowershellFunction<string, string> vDefaultPowershellFunction;
+        public PowershellFunction<string, string> DefaultValue {
+            get {
+                return vDefaultPowershellFunction ??
+                       (vDefaultPowershellFunction = new PowershellFunction<string, string> {
+                           FunctionName = "Encrypt-String",
+                           Script = "\r\n\tfunction Encrypt-String($s) {\r\n"
+                                    + "\t\t$result = New-Object -TypeName \"Aspenlaub.Net.GitHub.CSharp.Pegh.Entities.PowershellFunctionResult\"\r\n"
+                                    + "\t\t$result.Result = $s + \" - but do not tell anyone\"\r\n"
+                                    + "\t\treturn $result\r\n"
+                                    + "\t}\r\n"
+                       });
+            }
+        }
+
+        public string Guid { get { return "6F829821-0C10-47C1-A298-A8AECC456D37"; } }
+    }
+}
