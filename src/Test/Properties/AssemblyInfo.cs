1855490bc432ad0d82fc8415e63f94c4f3765467
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2018-10-21 11:52:09 +0200

    Project standardization

--- src/Test/Properties/AssemblyInfo.cs
+++ src/Test/Properties/AssemblyInfo.cs
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("02d4615b-ce40-42fb-a3d8-0ac01678f336")]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
+// [assembly: AssemblyFileVersion("1.0.0.0")]
