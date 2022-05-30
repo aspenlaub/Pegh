64f708b0342b72fa99d87bdbcf049b180861eeca
aspenlaub <wolfgang.berger@viperfisch.de>
2022-05-30 09:19:20 +0200

    Illustrate logging behaviour when several tasks start a scope

---
+++ src/Test/Components/ImLogging.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Aspenlaub.Net.GitHub.CSharp.Pegh.Entities;
+using Aspenlaub.Net.GitHub.CSharp.Pegh.Interfaces;
+using Microsoft.Extensions.Logging;
+
+namespace Aspenlaub.Net.GitHub.CSharp.Pegh.Test.Components {
+    public class ImLogging {
+        private readonly TimeSpan LogEvery;
+        private readonly DateTime EndOfWork;
+        private readonly ISimpleLogger SimpleLogger;
+        private readonly string WorkerId;
+
+        public ImLogging(TimeSpan logEvery, DateTime endOfWork, ISimpleLogger simpleLogger) {
+            LogEvery = logEvery;
+            EndOfWork = endOfWork;
+            SimpleLogger = simpleLogger;
+            WorkerId = Guid.NewGuid().ToString().Substring(0, 5);
+
+        }
+
+        public async Task Work() {
+            using (SimpleLogger.BeginScope(SimpleLoggingScopeId.Create(nameof(ImLogging), WorkerId))) {
+                SimpleLogger.LogInformation($"{WorkerId} starts working");
+                while (DateTime.Now < EndOfWork) {
+                    SimpleLogger.LogInformation($"{WorkerId} does something");
+                    await Task.Delay(LogEvery);
+                }
+            }
+        }
+    }
+}
--- src/Test/Components/SimpleLoggerTest.cs
+++ src/Test/Components/SimpleLoggerTest.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using Aspenlaub.Net.GitHub.CSharp.Pegh.Components;
 using Aspenlaub.Net.GitHub.CSharp.Pegh.Entities;
 using Aspenlaub.Net.GitHub.CSharp.Pegh.Interfaces;
@@ -20,6 +21,7 @@ public class SimpleLoggerTest {
     public void SimpleLogger_WithoutScope_ThrowsException() {
         var flusher = new SimpleLogFlusher();
         ISimpleLogger sut = new SimpleLogger(flusher);
+        sut.LogSubFolder = $"AspenlaubLogs\\{nameof(SimpleLogger_WithoutScope_ThrowsException)}";
         sut.Log(LogLevel.Information, new EventId(0), new Dictionary<string, object>(), null, (_, _) => { return NotAMessage; });
     }
 
@@ -27,6 +29,7 @@ public class SimpleLoggerTest {
     public void SimpleLogger_WithManyLogCalls_IsWorking() {
         var flusher = new SimpleLogFlusher();
         ISimpleLogger sut = new SimpleLogger(flusher);
+        sut.LogSubFolder = $"AspenlaubLogs\\{nameof(SimpleLogger_WithManyLogCalls_IsWorking)}";
         using (sut.BeginScope(SimpleLoggingScopeId.Create("Scope", "A"))) {
             using (sut.BeginScope(SimpleLoggingScopeId.Create("Scope", "B"))) {
                 for (var i = 0; i < NumberOfLogEntries; i++) {
@@ -52,18 +55,29 @@ public class SimpleLoggerTest {
 
         File.SetLastWriteTime(fileName, DateTime.Now.AddHours(-25));
         SimpleLogFlusher.ResetCleanupTime();
-        flusher.Flush(sut, @"AspenlaubLogs\Miscellaneous");
+        flusher.Flush(sut, sut.LogSubFolder);
         Assert.IsFalse(File.Exists(fileName));
     }
 
     [TestMethod]
-    public void CanResolveInstance() {
-        var container = new ContainerBuilder().UsePegh("Pegh", new DummyCsArgumentPrompter()).Build();
-        var logger = container.Resolve<ILogger>();
+    public void PeghContainer_ReturnsSimpleLogger() {
+        var sut = new ContainerBuilder().UsePegh("Pegh", new DummyCsArgumentPrompter()).Build();
+        var logger = sut.Resolve<ILogger>();
         Assert.IsNotNull(logger);
         Assert.IsTrue(logger is SimpleLogger);
 
-        var simpleLogger = container.Resolve<ISimpleLogger>();
+        var simpleLogger = sut.Resolve<ISimpleLogger>();
         Assert.IsNotNull(simpleLogger);
+    }
+
+    [TestMethod]
+    public async Task SimpleLogger_WithParallelTasks_IsWorking() {
+        var flusher = new SimpleLogFlusher();
+        ISimpleLogger sut = new SimpleLogger(flusher);
+        sut.LogSubFolder = $"AspenlaubLogs\\{nameof(SimpleLogger_WithParallelTasks_IsWorking)}";
+        var timeSpans = new List<TimeSpan> { TimeSpan.FromMilliseconds(77), TimeSpan.FromMilliseconds(222), TimeSpan.FromMilliseconds(444) };
+        var endsOfWork = new List<DateTime> { DateTime.Now.AddSeconds(4), DateTime.Now.AddSeconds(2.4), DateTime.Now.AddSeconds(7) };
+        var tasks = new List<Task>(timeSpans.Select((t, i) => new ImLogging(t, endsOfWork[i], sut).Work()));
+        await Task.WhenAll(tasks);
     }
 }
