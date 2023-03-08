7cb83bbaa63907b87abc37fa1f477cef6e473091
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2023-03-08 08:27:06 +0100

    Update runtests.cmd

--- runtests.cmd
+++ runtests.cmd
@@ -1,3 +1,5 @@
 dotnet cake build.cake --Target=DebugBuild
 dotnet cake build.cake --Target=RunTestsOnDebugArtifacts
+dotnet cake build.cake --Target=ReleaseBuild
+dotnet cake build.cake --Target=RunTestsOnReleaseArtifacts
 pause
