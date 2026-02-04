94f74f73a65ccb2d2f8c8081541fcb887e3ee999
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2026-02-04 17:45:10 +0100

    Adjust unit test

--- src/Test/AutofacVersionTest.cs
+++ src/Test/AutofacVersionTest.cs
@@ -5,7 +5,7 @@ namespace Aspenlaub.Net.GitHub.CSharp.Pegh.Test;
 
 [TestClass]
 public class AutofacVersionTest {
-    private const string _expectedVersion = "7.0.1.0";
+    private const string _expectedVersion = "9.0.0.0";
 
     [TestMethod]
     public void PeghUsesRightAutofacPackageVersion() {
