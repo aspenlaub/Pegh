750aa9eaa2017e2aeee75c3f891163126f188fad
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2026-02-14 15:39:21 +0100

    Cake is history

--- runtests.cmd
+++
@@ -1,5 +0,0 @@
-dotnet cake build.cake --Target=DebugBuild
-dotnet cake build.cake --Target=RunTestsOnDebugArtifacts
-dotnet cake build.cake --Target=ReleaseBuild
-dotnet cake build.cake --Target=RunTestsOnReleaseArtifacts
-pause
--- solution.cake
+++
@@ -1,4 +0,0 @@
-var solution = "./src/Pegh.slnx";
-var masterDebugBinFolder = "../PeghBin/Debug";
-var masterReleaseBinFolder = "../PeghBin/Release";
-var solutionSpecialSettingsDictionary = new Dictionary<string, string>();
